c589bd6653150e60261c1dfe2d6d3c6f8078321d
owo <uwu@owo.computer>
2026-04-22 14:48:25 +0200

    feat: fixed dell fingerprint sensor

--- hosts/dell/configuration.nix
+++ hosts/dell/configuration.nix
@@ -1,8 +1,5 @@
 { config, pkgs, lib, ... }:
 
-let 
-    goodix-driver = pkgs.callPackage ./goodix53x5.nix { };
-in    
 {
   imports = [
     ../../modules/all.nix
@@ -12,49 +9,13 @@ in
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
   networking.hostName = "dell";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Enable networking
-  # networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-
-  #i18n.extraLocaleSettings = {
-  #  LC_ADDRESS = "de_DE.UTF-8";
-  #  LC_IDENTIFICATION = "de_DE.UTF-8";
-  #  LC_MEASUREMENT = "de_DE.UTF-8";
-  #  LC_MONETARY = "de_DE.UTF-8";
-  #  LC_NAME = "de_DE.UTF-8";
-  #  LC_NUMERIC = "de_DE.UTF-8";
-  #  LC_PAPER = "de_DE.UTF-8";
-  #  LC_TELEPHONE = "de_DE.UTF-8";
-  #  LC_TIME = "de_DE.UTF-8";
-  #};
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  #services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  # services.displayManager.sddm.enable = true;
-  # services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "altgr-intl";
   };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -62,63 +23,27 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-#  users.users.${config.mainUser} = {
-#    packages = with pkgs; [
-#      kdePackages.kate
-#    ];
-#  };
-  
   services.fprintd = {
     enable = true;
-    tod = {
-      enable = true;
-      driver = goodix-driver;
+    # Point fprintd at our custom libfprint.
+    package = pkgs.fprintd.override {
+      libfprint = pkgs.libfprint-goodix53x5;
     };
   };
+
+  # Register udev rules from the patched libfprint
+  services.udev.packages = [ pkgs.libfprint-goodix53x5 ];
+
+  # Aggressive power management rules for Goodix Fingerprint Reader
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTR{idVendor}=="27c6", ATTR{idProduct}=="5385", ATTR{power/control}="on", ATTR{power/persist}="1", ENV{ID_AUTOSUSPEND}="0"
+  '';
+
   security.pam.services.login.fprintAuth = lib.mkForce true;
   security.pam.services.sudo.fprintAuth = lib.mkForce true;
   security.pam.services.gdm-fingerprint.fprintAuth = lib.mkForce true;
 
-  # Install firefox.
-  # programs.firefox.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  system.stateVersion = "25.11";
 }
