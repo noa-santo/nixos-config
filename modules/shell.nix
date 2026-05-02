458fcf874b306d1581de1a72c017a73bf8922ab3
owo <uwu@owo.computer>
2026-05-02 18:25:59 +0200

    fix: rebuild-reboot

--- modules/shell.nix
+++ modules/shell.nix
@@ -3,8 +3,20 @@
  environment.shellAliases = {
   rebuild = "sudo nixos-rebuild switch --flake $HOME/.config/nixos-config#$(hostname)";
   rebuild-fast = "sudo nixos-rebuild switch --flake $HOME/.config/nixos-config#$(hostname) --option substituters 'https://cache.nixos.org https://nix-community.cachix.org' --option trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURgN8cP5tPgkpXYAOGOBo= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs='";
-  rebuild-reboot = "sudo nixos-rebuild boot --flake $HOME/.config/nixos-config#$(hostname) && read -p 'Nixos rebuild successful. Reboot now? (y/N) ' -n 1 -r && echo && [[ $REPLY =~ ^[yY]$ ]] && sudo reboot";
   update = "sudo nix flake update --flake $HOME/.config/nixos-config";
  };
- programs.fish.enable = true;
+
+ environment.systemPackages = with pkgs; [
+   (pkgs.writeShellScriptBin "rebuild-reboot" ''
+    sudo nixos-rebuild boot --flake $HOME/.config/nixos-config#$(hostname)
+    if [ $? -eq 0 ]; then
+     read -p "Nixos rebuild successful. Reboot now? (y/N) " response
+     if [[ "$response" = "y" || "$response" = "Y" ]]; then
+      sudo reboot
+     fi
+    fi
+   '')
+  ];
+
+  programs.fish.enable = true;
 }
