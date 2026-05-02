280d9d8d78fd960fa2b3632153ce93168c80fc5a
owo <uwu@owo.computer>
2026-05-02 19:49:43 +0200

    fix: correct pub key

--- modules/shell.nix
+++ modules/shell.nix
@@ -2,7 +2,7 @@
 {
  environment.shellAliases = {
   rebuild = "sudo nixos-rebuild switch --flake $HOME/.config/nixos-config#$(hostname)";
-  rebuild-fast = "sudo nixos-rebuild switch --flake $HOME/.config/nixos-config#$(hostname) --option substituters 'https://cache.nixos.org https://nix-community.cachix.org' --option trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURgN8cP5tPgkpXYAOGOBo= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs='";
+  rebuild-fast = "sudo nixos-rebuild switch --flake $HOME/.config/nixos-config#$(hostname) --option substituters 'https://cache.nixos.org https://nix-community.cachix.org' --option trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs='";
   update = "sudo nix flake update --flake $HOME/.config/nixos-config";
   # TODO: cleanup command that deletes unused stuff and old config backups
  };
