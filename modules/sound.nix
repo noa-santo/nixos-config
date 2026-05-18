5fbcbff3824eeb9b8832d9015f5b1ea5b99f1b81
owo <uwu@owo.computer>
2026-05-18 21:59:25 +0200

    feat: better bluetooth quality

--- modules/sound.nix
+++ modules/sound.nix
@@ -7,5 +7,22 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    wireplumber.extraConfig."10-bluetooth-policy" = {
+        "wireplumber.settings" = {
+          "bluetooth.autoswitch-to-headset-profile" = false;
+        };
+      };
+
+      wireplumber.configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/11-bluetooth-policy.conf" ''
+          monitor.bluez.properties = {
+            bluez5.enable-sbc-xq = true
+            bluez5.enable-msbc = true
+            bluez5.enable-hw-volume = true
+            bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source ]
+          }
+        '')
+      ];
   };
 }
